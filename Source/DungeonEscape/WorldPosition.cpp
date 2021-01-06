3cbb8cd74ce35affef92ab64d29ac8e882d0485b
Fletchlock <47433327+Fletchlock@users.noreply.github.com>
2021-01-06 13:06:28 -0700

    Update WorldPosition.cpp

--- Source/DungeonEscape/WorldPosition.cpp
+++ Source/DungeonEscape/WorldPosition.cpp
@@ -24,7 +24,7 @@ void UWorldPosition::BeginPlay()
 	
 	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
 
-	const FVector NewLocation = FVector(-80, 190, 70);
+	const FVector NewLocation = FVector(-80, 190, 90);
 	
 	UE_LOG(LogTemp, Warning, TEXT("%s location : %s"), *ObjectName, *ObjectPosition);
 
