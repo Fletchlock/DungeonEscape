abdb758882231b79d14e044c1cbabec61c4881f3
Fletchlock <47433327+Fletchlock@users.noreply.github.com>
2021-01-11 14:58:20 -0700

    VH
    - Added DrawDebugLine to test for line trace in future

--- Source/DungeonEscape/Grabber.cpp
+++ Source/DungeonEscape/Grabber.cpp
@@ -1,6 +1,7 @@
 // Copyright GreyHone Games
 
 #include "Grabber.h"
+#include "DrawDebugHelpers.h"
 #include "GameFramework/PlayerController.h"
 #include "Engine/World.h"
 
@@ -33,26 +34,40 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// Get players viewpoint
-
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
 		OUT PlayerViewPointLocation,
 		OUT PlayerViewPointRotation
-		);
+	);
 
 	// Logging out to test
-
 	//FString Location = PlayerViewPointLocation.ToString();
 	//FString Rotation = PlayerViewPointRotation.ToString();
-
 	UE_LOG(LogTemp, Warning, TEXT("Players ViewPoint Location : %s Players ViewPoint Rotation : %s"),
 		*PlayerViewPointLocation.ToString(),
 		*PlayerViewPointRotation.ToString()
-		);
+	);
+
+	//Set variables for line trace
+	
+	FColor TraceColor = { 255, 0, 0 };
+	FVector LineStart = PlayerViewPointLocation;
+	FVector LineEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Range;
 
 	// Ray-cast (line trace) out to certain distance (Reach)
+	DrawDebugLine(
+		GetWorld(),
+		LineStart,
+		LineEnd,
+		TraceColor,
+		false,
+		-1.f,
+		0,
+		2.f
+	);
+
 
 	// See what we are hitting
 
